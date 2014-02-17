60d2872af653d3c54805b3f2e5ddee285cd3e601
luis <lrosario101@gmail.com>
2014-02-17 02:19:47 -0500

    getting ready to add multiple buttons

--- simon.ino
+++ simon.ino
@@ -11,33 +11,48 @@ const int red = 4;       // error/wrong
 // led array
 int ledPins[] = { green, yellow, red, LED };
 int ledTotal = 4;        // is there no array.length attr in arduino?..
+int gamePins[] = { green, yellow, red };
+int gamePinsTotal = 3;
 
 // set pin numbers:
-const int button = 8;
+const int redBtn = 8;
+const int yellowBtn = 9;
+const int greenBtn = 10;
 
 // helper vars
 int buttonState = 0;     // pushbutton status
 String state = "idle";   // game state
+
+// game variables
 int gameRound = 1;
+int pattern[] = { green };
+int playerPattern[] = { green };
 
 
 
 
 // setup
 // ===================================
-void setup() {                
+void setup() {
   // initialize the LED pins (loop through LEDs array)
   for(int led = 0; led < ledTotal; led++) {
     pinMode(ledPins[led], OUTPUT);
   }
   
-  // initialize button
-  pinMode(button, INPUT);
+  // initialize redBtn
+  pinMode(redBtn, INPUT);
+  // pinMode(yellowBtn, INPUT);
+  // pinMode(greenBtn, INPUT);
   
   // serial read
   Serial.begin(9600);
-  Serial.println("yes, hello. this is Simon");
+  Serial.println("===================================");
+  Serial.println("|              SIMON              |");
+  Serial.println("===================================");
   Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println("yes, hello. this is Simon");
 }
 
 
@@ -46,45 +61,64 @@ void setup() {
 // loop
 // ===================================
 void loop() {
-  Serial.print("state: ");
-  Serial.print(state);
-
 
   // check game state
   // ----------------------------
   // game
   if(state == "simon"){
     // setting a new pattern
-    Serial.println(" | start new game");
-    alert();
     simon();
   }
 
   // idle
   if(state == "idle"){
     // read the state of the pushbutton value:
-    buttonState = digitalRead(button);
-    Serial.print(" | button: ");
-    Serial.println(buttonState);
+    buttonState = digitalRead(redBtn);
    
     // if btn is pressed -> start game
     if (buttonState == HIGH) {
+      alert();
       state = "simon";}
-    else {
-      knightRider();}
+    else { knightRider();} // idle LED state
   }
-
 }
 
 
 // game functions
 // ===================================
 void simon() {
+  // /* ---------------------------- */
+  // /             SIMON              /
+  // /* ---------------------------- */
+  // the game round determines the number of times each LED is turned on
+  //    round 1 - all LEDs will be turned on once
+  //    round 3 - all LEDs will be turned on 3 times
+  // the order in which they are turned on is randomized
+  // the player must match the pattern
+
   Serial.print("Round: ");
   Serial.println(gameRound);
+
+  // set pattern
+  showPattern();
+
+  // bring us back to Simon
+  state = "simon";
 }
 
+void showPattern() {
+  // turn on LED
+  turnOn(LED);
 
+  // make pattern
+  int length = gameRound * gamePinsTotal;
+  for(int i=0; i<length; i++){
+    Serial.println(i+1);
+  }
+}
+
+void turnOn(int _led) {digitalWrite(_led, HIGH);}
+void turnOff(int _led) {digitalWrite(_led, LOW);}
 
 
 // light effects
@@ -128,3 +162,6 @@ void turnOffAllLEDs() {
     digitalWrite(ledPins[led], LOW);
   }
 }
+
+
+
